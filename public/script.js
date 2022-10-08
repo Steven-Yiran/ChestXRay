7ece4c62c03c334f66a407b4ab56a78ed9159c92
LukaBec <lbecerra@middlebury.edu>
2022-10-08 11:32:13 -0400

    Worked on implementing ML model
    Implemented code that trains model.
    Implemented code the views image upload.
    Need to create, convert, and load model.

--- public/script.js
+++ public/script.js
@@ -1,2 +1,19 @@
 //for running events and functions
+/*
+tf.loadLayersModel("jsmodel/model.json").then(model => {
+	this._model = model;
+})*/
+
+const fileSelector = document.getElementById('myFile');
+const output = document.getElementById('output');
+
+fileSelector.addEventListener('change', event => {
+    output.src = '';
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.addEventListener('load', event => {
+        output.src = event.target.result;
+      });
+    reader.readAsDataURL(file);
+});
 
