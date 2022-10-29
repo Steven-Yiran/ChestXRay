8654e87c9e6a3601ae6285222ee8360961661f6a
Steven-Yiran <60683228+Steven-Yiran@users.noreply.github.com>
2022-10-29 18:06:40 -0400

    Co-authored-by: LukaBec <LukaBec@users.noreply.github.com>

--- public/script_main.js
+++ public/script_main.js
@@ -2,7 +2,9 @@
 const fileSelector = document.getElementById('input-image');
 const output = document.getElementById('output');
 const diagOutput = document.getElementById("diag_info");
+const formElement = document.getElementById("image-frm");
 
+// upload preview
 fileSelector.addEventListener('change', async event => {
   output.src = '';
     const image = event.target.files[0];
@@ -21,28 +23,26 @@ fileSelector.addEventListener('change', async event => {
 });
 
 
-function predict() {
-  // Load the model.
-  mobilenet.load().then(model => {
-    // Classify the image.
-    model.classify(output).then(predictions => {
-      console.log('Predictions:');
-      console.log(predictions);
-      res = "";
-      for (let val = 0; val < predictions.length; val++) {
-         res += predictions[val]["className"] + ": " + Number(predictions[val]["probability"].toPrecision(3)) + "\n";
-      }
-      diagOutput.innerHTML = res;
-    });
-  });
+// Based on:
+// https://stackoverflow.com/questions/46640024/how-do-i-post-form-data-with-fetch-api
+function postImage() {
+  const data = new URLSearchParams();
+  for (const pair of new FormData(formElement)) {
+    data.append(pair[0], pair[1]);
+  }
+  console.log("here!");
+  console.log(data);
+  // const url = "/predict";
+  // const response = await fetch(url, {
+  //   method: 'post',
+  //   body: data,
+  // })
+  // const body = await response.json();
+  // console.log(body);
 }
+
 
 function resetImage() {
   output.src = '';
   diagOutput.innerHTML = "Diagnosis information will be displayed here...";
 }
-
-/*
-tf.loadLayersModel("jsmodel/model.json").then(model => {
-	this._model = model;
-})*/
