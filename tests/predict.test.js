3d7654b05dd96de6af8aedc1918ca365704fe7f5
Steven-Yiran <60683228+Steven-Yiran@users.noreply.github.com>
2022-11-03 10:46:17 -0400

    Wrote unit test

---
+++ tests/predict.test.js
@@ -0,0 +1,9 @@
+const { test_util } = require("@tensorflow/tfjs-node");
+const {inferenceController} = require("../controllers/predict");
+
+test('check model is not null', async () => {
+    await inferenceController.ensureModelLoaded();
+    expect(inferenceController.model).not.toBeNull();
+})
+
+// write an unit test on post api
